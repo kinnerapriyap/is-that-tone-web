cf6b9b394798d5424552a2d39e9dd25a907318b6
kinnerapriyap <kinnerapriyap@gmail.com>
2021-03-21 20:39:17 +0900

    Add TagInput to screen

--- src/components/IsThatTone.vue
+++ src/components/IsThatTone.vue
@@ -1,19 +1,55 @@
 <template>
   <h1>hi {{ userEmail }}</h1>
-  <button @click="submit">Submit</button>
+  <div class="column">
+    <div class="row">
+      Word:
+      <input v-model.trim="word" placeholder="Enter word" />
+    </div>
+    <div class="row">
+      Options:
+      <TagInput v-model="options" />
+    </div>
+    <button @click="submit">Submit</button>
+  </div>
 </template>
 
 <script setup>
+import { reactive, ref } from "vue";
+import firebase from "firebase/app";
+import TagInput from "./TagInput.vue";
+
+const word = ref("");
+const options = reactive({});
+
 defineProps({
   userEmail: String,
 });
+
 const submit = () => {
   console.log("submitted");
+  console.log(word.value);
+  console.log(options);
 };
 </script>
 
 <style scoped>
-a {
-  color: #42b983;
+button,
+input {
+  margin: 5px;
+  padding: 0 10px;
+  box-sizing: border-box;
+  border: 1px solid #eee;
+  font-size: 0.9em;
+  height: 50px;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 </style>
