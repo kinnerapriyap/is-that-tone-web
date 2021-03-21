ecf8a3b2d7f4f5c75b4d78a017fc772d126189e3
kinnerapriyap <kinnerapriyap@gmail.com>
2021-03-21 11:55:22 +0900

    Add sign in with email firebaseui auth

---
+++ src/components/Auth.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Welcome to is-that-tone-web</h1>
+  <div id="firebaseui-auth-container"></div>
+  <div id="loader">Loading...</div>
+</template>
+
+<script setup>
+import firebase from "firebase/app";
+import "firebaseui";
+import "firebase/auth";
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function () {
+      document.getElementById("loader").style.display = "none";
+    },
+  },
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+};
+ui.start("#firebaseui-auth-container", uiConfig);
+</script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import firebase from "firebase/app";
-import "firebase/auth";
 
 firebase.initializeApp(firebaseConfig);
-firebase
-  .auth()
-  .signInAnonymously()
-  .then(() => {
-    return console.log("signed in");
-  })
-  .catch((error) => console.error(error));
 createApp(App).mount("#app");
