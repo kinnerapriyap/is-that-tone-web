856e48c55ed433cfac675ce8d31de8b7edf4eeca
kinnerapriyap <kinnerapriyap@gmail.com>
2021-03-25 06:54:50 +0900

    Loading hm

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Brainstorm" src="./assets/brainstorm.png" width="100" />
-  <Loading v-if="!isSignedIn" />
-  <IsThatTone v-else-if="isSignedIn" :userEmail="userEmail" />
+  <!--Loading v-if="!isSignedIn" /-->
+  <IsThatTone v-if="isSignedIn" :userEmail="userEmail" />
   <Auth v-else />
 </template>
 
